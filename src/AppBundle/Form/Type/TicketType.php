123d92ea19dffcbe0c2fa66530e27a4dce57cd27
Aurelien Morvan <morvan.aurelien@gmail.com>
2017-03-20 20:13:41 +0100

    [++][UP][Form] Finish create TicketType model with adding all input & add unit test for this

--- src/AppBundle/Form/Type/TicketType.php
+++ src/AppBundle/Form/Type/TicketType.php
@@ -4,8 +4,14 @@ namespace AppBundle\Form\Type;
 
 use AppBundle\Entity\Ticket;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 /**
  * Class TicketType
@@ -19,7 +25,54 @@ class TicketType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        //TODO construction formulaire
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom :',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le champ prénom doit être renseigné',
+                    ]),
+                    new Length([
+                        'max' => 38,
+                        'maxMessage' => 'Le champ prénom dépasse 38 caractères',
+                    ]),
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom :',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le champ nom doit être renseigné',
+                    ]),
+                    new Length([
+                        'max' => 38,
+                        'maxMessage' => 'Le champ nom dépasse 38 caractères',
+                    ]),
+                ],
+            ])
+            ->add('birthDate', BirthdayType::class, [
+                'label' => 'Votre date de naissance :',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le champ date de naissance doit être renseigné',
+                    ]),
+                ],
+            ])
+            ->add('nationality', CountryType::class, [
+                'label' => 'Votre nationalité :',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le champ nationalité doit être renseigné',
+                    ]),
+                ],
+            ])
+            ->add('reduction', CheckboxType::class, [
+                'label' => 'Disposez-vous d\'un tarif réduit, un justificatif vous sera demandé à l\'entrée du musée',
+            ]);
     }
 
     /**
