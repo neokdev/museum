859bb3c22d57d73495c49951f7f6e7ab45c446c3
Aurélien Morvan <contact@aurelien-morvan.fr>
2017-03-26 20:47:25 +0200

    [++][ADD][Service] Add new service to calculate total order price

--- src/AppBundle/Services/TicketManager.php
+++ src/AppBundle/Services/TicketManager.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Services;
 
-use AppBundle\Entity\Order;
 use AppBundle\Entity\Ticket;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -19,12 +18,6 @@ class TicketManager
 
     /** @var Session */
     private $session;
-
-    /** @var int */
-    private $priceTicket;
-
-    /** @var int */
-    private $totalPrice;
 
     /**
      * TicketManager constructor.
@@ -51,67 +44,5 @@ class TicketManager
                 'Il reste moins de 100 billets disponible sur la journée en cours, ne perdez pas de temps'
             );
         }
-    }
-
-    /**
-     * @param Order $order
-     *
-     * @return Order
-     */
-    public function getTotalPriceOrder(Order $order)
-    {
-        $tickets = $order->getTickets();
-        foreach ($tickets as $ticket) {
-            try {
-                if (!is_object($ticket->getBirthDate() && !is_object($order->getDateVisit()))) {
-                    throw new \InvalidArgumentException(
-                        sprintf(
-                            'Must be an object, %s given',
-                            gettype([$ticket->getBirthDate(), $order->getDateVisit()])
-                        )
-                    );
-                }
-            } catch (\InvalidArgumentException $exception) {
-                $exception->getMessage();
-            }
-
-            $age = date_diff($ticket->getBirthDate(), $order->getDateVisit())->y;
-            $priceTicket = $this->setPriceForEachTicket($ticket->isReduction(), $age);
-            $ticket->setPrice($priceTicket);
-
-            $this->totalPrice += $ticket->getPrice();
-            $order->setTotalPrice($this->totalPrice);
-        }
-
-        return $order;
-    }
-
-    /**
-     * @param $reduction
-     * @param $age
-     *
-     * @return int
-     */
-    private function setPriceForEachTicket($reduction, $age)
-    {
-        switch ($age) {
-            case $age < 4:
-                $this->priceTicket = 0;
-                break;
-            case $age > 4 && $age < 12:
-                $this->priceTicket = 8;
-                break;
-            case $reduction:
-                $this->priceTicket = 10;
-                break;
-            case $age >= 60:
-                $this->priceTicket = 12;
-                break;
-            default:
-                $this->priceTicket = 16;
-                break;
-        }
-
-        return $this->priceTicket;
     }
 }
