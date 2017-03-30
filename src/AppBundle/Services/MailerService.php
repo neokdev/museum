408473d2fa5724743245a80a5c87b49576e2896e
Aurélien Morvan <contact@aurelien-morvan.fr>
2017-03-30 13:12:47 +0200

    [++][Fix][Service/Mailer] Fix mailer service

--- src/AppBundle/Services/MailerService.php
+++ src/AppBundle/Services/MailerService.php
@@ -65,7 +65,7 @@ class MailerService
         $mail = \Swift_Message::newInstance()
             ->setSubject($subject)
             ->setFrom('musee@aurelien-morvan.fr')
-            ->setTo('morvan.aurelien@gmail.com', 'Musee du Louvre')
+            ->setTo('morvan.aurelien@gmail.com', $this->nameEmailMuseum)
             ->setBody(
                 $this->templating->render(
                     $templateMail,
